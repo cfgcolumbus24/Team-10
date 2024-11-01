543af555f06e97183d17f72c8fd2f2b32c46d299
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-11-01 18:47:55 -0400

    build: update schema for session handling

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
 import {
+    boolean,
     integer,
     pgEnum,
     pgTable,
     text,
     timestamp,
-    uniqueIndex,
 } from "drizzle-orm/pg-core";
 
 import { sql } from "drizzle-orm";
@@ -12,10 +12,28 @@ import { sql } from "drizzle-orm";
 export const users = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     email: text().notNull().unique(),
-    passwordHash: text().notNull(),
+    passwordHash: text("password_hash").notNull(), // update when a password reset request is defeated
     name: text().notNull().unique(),
     bio: text().notNull(),
     contact: text().notNull(),
+});
+
+export const sessions = pgTable("sessions", {
+    token: text().notNull().primaryKey(), // generated using CSPRNG
+
+    userId: integer("user_id")
+        .notNull()
+        .references(() => users.id),
+
+    ipAddr: text("ip_addr"),
+    userAgent: text("user_agent"),
+
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    expiresAt: timestamp("expires_at")
+        .notNull()
+        .default(sql`CURRENT_TIMESTAMP + INTERVAL '1 week'`),
+
+    invalidated: boolean().notNull().default(false),
 });
 
 export const follows = pgTable("follows", {
