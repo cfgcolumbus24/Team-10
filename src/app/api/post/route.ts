9c6ddec7ccc36352522b07df99c342c1fbeba1c5
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-11-01 18:31:35 -0400

    build: add example route for post

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,52 @@
+import { ApiResponse } from "@/app/api/common";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const PostSchema = z.object({
+    image: z.string(),
+    body: z.string(),
+    type: z.enum(["post", "opportunity", "event"]),
+});
+
+export async function POST(
+    request: Request
+): Promise<NextResponse<ApiResponse>> {
+    try {
+        const body = await request.json();
+
+        const result = PostSchema.safeParse(body);
+        if (!result.success) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    error: "Invalid data",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
+        const validatedData = result.data;
+
+        return NextResponse.json(
+            {
+                success: true,
+                data: validatedData,
+            },
+            {
+                status: 201,
+            }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Failed to process request",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
