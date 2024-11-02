ae96f9ed0a4d7ab06eb387170954a2aac41ac71f
Emerson Frasure <emersonfrasure@gmail.com>
2024-11-02 07:59:13 -0400

    Gallery Card add

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -119,6 +119,7 @@ export default function Index() {
                                 </Card>
                             </a>
                         ))}
+                        <GalleryCard />
                     </div>
                 </div>
             </div>
