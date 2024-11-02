f3f77c1a430ab3c59d7471f07ca1b97f5a4e9002
Christian J Wilkins <wilki385@msu.edu>
2024-11-01 20:53:41 -0400

    Commiting card WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import Navbar from "../../components/Navbar";
+import Navbar from "../components/ui/Navbar";
+import { Card, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Navbar />
+      <div className="flex-auto content-center items-center justify-center flex">
+      <Card className="w-[40%]">
+        <CardHeader>Hello</CardHeader>
+      </Card>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -100,6 +104,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    </div>
     </div>
     </div>
   );
