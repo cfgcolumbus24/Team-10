b8f06afe4a660f9e456810d6cfa4d2ad431825d7
Elharith Madani <harithmadani54@gmail.com>
2024-11-02 02:15:43 -0400

    added mobile responsiveness

--- src/components/ui/ProfileCard.tsx
+++ src/components/ui/ProfileCard.tsx
@@ -17,31 +17,29 @@ export default function ProfileCard({
   bio
 }: ProfileCardProps) {
   return (
-    <section className="w-3/4 mx-auto p-4 bg-white rounded-lg shadow-md border border-gray-200 flex items-center relative space-x-4 mt-4">
+    <section className="w-3/4 sm:w-full mx-auto p-4 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col sm:flex-row items-center relative space-y-4 sm:space-y-0 sm:space-x-4 mt-4">
       {/* Profile Picture */}
       <div className="flex-shrink-0">
-        <Avatar className="items-center justify-center align-center w-32 h-32">
+        <Avatar className="items-center justify-center w-24 h-24 sm:w-32 sm:h-32">
           <AvatarImage src="https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_1.png" className="rounded-full object-cover"/>
           <AvatarFallback>PFP</AvatarFallback>
         </Avatar>
-
       </div>
 
       {/* Profile Details */}
-      <div className="flex-1">
-        <div className="flex items-baseline justify-between mb-2">
+      <div className="flex-1 text-center sm:text-left">
+        <div className="flex flex-col sm:flex-row items-center sm:items-baseline justify-between mb-2">
           <div>
-            <p className="text-xl font-semibold">{firstName} {lastName}</p>
-            <p className="text-base text-gray-500">{profession}</p>
+            <p className="text-lg sm:text-xl font-semibold">{firstName} {lastName}</p>
+            <p className="text-sm sm:text-base text-gray-500">{profession}</p>
           </div>
-          
         </div>
         {/* Bio */}
         <p className="text-gray-700 text-sm">{bio}</p>
-        
       </div>
+      
       {/* Add Friend Button */}
-      <button className="bg-[#1CBCEE] text-white py-1.5 px-3 rounded-md hover:bg-[#F3686B] flex items-start self-start space-x-1">
+      <button className="bg-[#1CBCEE] text-white py-1.5 px-3 rounded-md hover:bg-[#F3686B] flex items-center space-x-1 mt-2 sm:mt-0 self-center sm:self-start">
         <MdPersonAdd className="text-lg" />
         <span>Add</span>
       </button>
