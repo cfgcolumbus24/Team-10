f3f659f7f64ead9b09c5cd50257496f13269e07a
Emerson Frasure <emersonfrasure@gmail.com>
2024-11-02 00:11:49 -0400

    Contact Card Icons

--- src/components/ui/ContactCard.tsx
+++ src/components/ui/ContactCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 
 interface ContactCardProps {
@@ -11,9 +13,16 @@ export default function ContactCard({
     phone
 }: ContactCardProps) {
     return (
-        <div className="w-1/4 mx-auto p-4 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col items-center justify-center gap-y-8 relative space-x-4 mt-4">
-            <p><a href={`mailto:${email}`}>{email}</a></p>
-            <p><a href={`tel:${phone}`}>{phone}</a></p>
+        <div className="w-1/4 p-4 pl-12 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col justify-center gap-y-8 relative mt-4">
+            <div className="flex items-center gap-x-2">
+                <FaPhoneAlt className="mr-2"/>
+                <p className="m-0"><a href={`mailto:${email}`}>{email}</a></p>
+            </div>
+
+            <div className="flex items-center gap-x-2">
+                <IoMdMail className="mr-2"/>
+                <p className="m-0 mx-0"><a href={`tel:${phone}`}>{phone}</a></p>
+            </div>  
         </div>
     )
 }
